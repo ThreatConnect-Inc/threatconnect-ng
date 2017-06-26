166bf21df9f6f4a74816d94d67f6bdb476b3de48
Bracey Summers <bsummers@threatconnect.com>
2017-06-26 23:50:10 +0000

    - cleanup

--- lib/tc_group.service.ts
+++ lib/tc_group.service.ts
@@ -50,7 +50,6 @@ export class TcGroupService {
         resultLimit: number = 500,
         resultStart: number = 0,
         owner: any = undefined,
-        // normalize: boolean = false,
         type: string = 'Group'
     ): Observable<any> {
         this.logging.debug('owner', owner);
@@ -87,82 +86,6 @@ export class TcGroupService {
             this.handleError);
     }
 
-    // private normalize(
-    //     response: Response,
-    //     type: any
-    // ) {
-    //     let data = JSON.parse(response.text(), function(prop, value) {
-    //         var fileString = ' : : ';
-    //         switch (prop) {
-    //             case 'address':
-    //                 if (type.type === 'EmailAddress') {
-    //                     this.summary = value;
-    //                     this.type = 'EmailAddress';
-    //                     return value;
-    //                 }
-    //                 else {
-    //                     this.indicator = value;
-    //                     return;
-    //                 }
-    //             case 'emailAddress':
-    //                 this.indicator = value;
-    //                 return value;
-    //             case 'file':
-    //                 this.indicator = value;
-    //                 return;
-    //             case 'host':
-    //                 this.indicator = value;
-    //                 return;
-    //             case 'hostName':
-    //                 this.summary = value;
-    //                 this.type = 'Host';
-    //                 return value;
-    //             case 'ip':
-    //                 this.summary = value;
-    //                 this.type = 'Address';
-    //                 return value;
-    //             case 'md5':
-    //                 this.summary = fileString
-    //                     .replace(/^ : /, value.toUpperCase() + ' : ');
-    //                 this.type = 'File';
-    //                 return value.toUpperCase();
-    //             case 'sha1':
-    //                 if (this.md5) {
-    //                     fileString = fileString
-    //                         .replace(/^ : /, this.md5 + ' : ');
-    //                 }
-    //                 this.summary = fileString
-    //                     .replace(/ : : /, ' : ' + value.toUpperCase() + ' : ');
-    //                 this.type = 'File';
-    //                 return value.toUpperCase();
-    //             case 'sha256':
-    //                 if (this.md5) {
-    //                     fileString = fileString
-    //                         .replace(/^ : /, this.md5 + ' : ');
-    //                 }
-    //                 if (this.sha1) {
-    //                     fileString = fileString
-    //                         .replace(/ : : /, ' : ' + this.sha1 + ' : ');
-    //                 }
-    //                 this.summary = fileString
-    //                     .replace(/ : $/, ' : ' + value.toUpperCase());
-    //                 this.type = 'File';
-    //                 return value.toUpperCase();
-    //             case 'text':
-    //                 this.summary = value;
-    //                 this.type = 'URL';
-    //                 return value;
-    //             case 'url':
-    //                 this.indicator = value;
-    //                 return;
-    //             default:
-    //                 return value;
-    //         }
-    //     }).data;
-
-    //     return data;
-    // }
-    
     private handleError(error: any) {
         return Promise.reject(error);
     }
