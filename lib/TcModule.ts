7956d931937e6a6263137ae3568fcdfb41767e9a
Chris Blades <cblades@threatconnect.com>
2017-09-13 11:07:11 -0400

    remove spacesNg import

--- lib/TcModule.ts
+++ lib/TcModule.ts
@@ -7,12 +7,7 @@ import { TcOwnerService } from './tc_owner.service';
 import { TcSecurityLabelService } from './tc_security_labels.service';
 import { TcUtilityService } from './tc_utility.service';
 
-import { SpacesModule } from 'spaces-ng';
-
 @NgModule({
-    imports: [
-        SpacesModule,
-    ],
     providers: [
         TcExchangeDbService,
         TcGroupService,
